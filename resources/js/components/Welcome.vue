3c519c6294f650410a9c8d8679eadb08d1b30cf7
AmmarALHasanat <ammarabed13@gmail.com>
2022-09-22 11:44:36 +0300

    first commit

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="d-flex flex-column w-100 align-items-center text-warning p-5">
+        <h1 class="" >{{ title }}</h1>
+        <p class="fs-3 text">{{ message }}</p>
+    </div>
+
+
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: 'Vue, Laravel, and bootStrap ',
+        message: 'This is a simple example of how to use Vue 3 in Laravel 8 with Bootstrap 5.2.1'
+    })
+}
+</script>
